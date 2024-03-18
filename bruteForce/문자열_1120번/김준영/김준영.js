2bfba2f3c0ee66533f60fd62f3923a077178c609
yohan11 <yipco77@naver.com>
2024-03-18 16:17:51 +0900

    fix: solved 1120.문자열

--- bruteForce/문자열_1120번/김준영/김준영.js
+++ bruteForce/문자열_1120번/김준영/김준영.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-let input = fs.readFileSync(filePath).toString().split(" ");
+let input = fs.readFileSync(filePath).toString().trim().split(" ");
 
 const a = input[0];
 let b = input[1];
