e29a365a5eafa3f3e2670f205dd0b107bba3f879
ryugwanghyoen <rgh4422@google.com>
2024-04-01 14:29:10 +0900

    feat:베스트셀러_1302

--- sort/올림픽_8979/류광현/류광현.js
+++ sort/올림픽_8979/류광현/류광현.js
@@ -6,6 +6,7 @@ const [N, K] = input[0].split(" ").map(Number);
 const countries = input.slice(1).map((line) => line.split(" ").map(Number));
 
 let rank = 1;
+countries.sort((a, b) => a[0] - b[0]);
 
 for (let i = 0; i < N; i++) {
   if (countries[i][0] !== K) {
